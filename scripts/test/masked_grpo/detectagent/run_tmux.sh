0af347f140b7184b9f78f06472fd02fb97ea9d4f
Thorin215 <1216513790@qq.com>
2025-09-04 05:56:27 +0000

    change tmux.sh

--- scripts/test/masked_grpo/detectagent/run_tmux.sh
+++ scripts/test/masked_grpo/detectagent/run_tmux.sh
@@ -101,7 +101,7 @@ tmux send-keys -t "$TRAIN_SESSION" "python3 -m vagen.trainer.main_ppo \\
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=1 \\
     actor_rollout_ref.rollout.tensor_model_parallel_size=2 \\
     actor_rollout_ref.rollout.name=vllm \\
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.45 \\
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.9 \\
     actor_rollout_ref.rollout.enable_chunked_prefill=False \\
     actor_rollout_ref.rollout.enforce_eager=False \\
     actor_rollout_ref.rollout.free_cache_engine=False \\
@@ -120,9 +120,9 @@ tmux send-keys -t "$TRAIN_SESSION" "python3 -m vagen.trainer.main_ppo \\
     algorithm.kl_ctrl.kl_coef=0.001 \\
     trainer.critic_warmup=0 \\
     trainer.logger=['console','wandb'] \\
-    trainer.project_name='vagen_7b_full_tool_step_data' \\
+    trainer.project_name='vagen_7b_full_tool_step3' \\
     trainer.experiment_name=$EXPERIMENT_NAME \\
-    trainer.n_gpus_per_node=2 \\
+    trainer.n_gpus_per_node=4 \\
     trainer.nnodes=1 \\
     trainer.save_freq=15 \\
     trainer.test_freq=3000 \\
